2167f2276c77f4f329667a7879742e3c1e2c5e48
Pau Perez <saulopefa@gmail.com>
2020-10-16 14:49:41 +0200

    Configure Carrierwave to use Blackblaze storage
    This makes Carrierwave, the asset storage library Decidim uses, with Backblaze's B2 cloud storage.
    
    I had a bit of trouble configuring it. Going through Fog AWS I realized
    that `:endpoint` is just an alternative to providing `:host`, `:scheme`
    and `:port`.
    
    It must be that the `@endpoint` set in
    fog-aws-3.6.7/lib/fog/aws/storage.rb is used elsewhere because replacing
    `:endpoint` with these other config vars doesn't work, although their
    equivalent ivars do end up with the same value.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,23 +9,25 @@ CarrierWave.configure do |config|
   config.enable_processing = !Rails.env.test?
 end
 
-# Setup CarrierWave to use Amazon S3. Add `gem "fog-aws" to your Gemfile.
-#
-# CarrierWave.configure do |config|
-#   config.storage = :fog
-#   config.fog_provider = 'fog/aws'                                             # required
-#   config.fog_credentials = {
-#     provider:              'AWS',                                             # required
-#     aws_access_key_id:     Rails.application.secrets.aws_access_key_id,     # required
-#     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
-#     region:                'eu-west-1',                                       # optional, defaults to 'us-east-1'
-#     host:                  's3.example.com',                                  # optional, defaults to nil
-#     endpoint:              'https://s3.example.com:8080'                      # optional, defaults to nil
-#   }
-#   config.fog_directory  = 'name_of_directory'                                 # required
-#   config.fog_public     = false                                               # optional, defaults to true
-#   config.fog_attributes = {
-#     'Cache-Control' => "max-age=#{365.day.to_i}",
-#     'X-Content-Type-Options' => "nosniff"
-#   }
-# end
+# Although we use Backblaze's B2 as object storage for assets, we use its S3's compatible API. We
+# briefly tried the `fog-backblaze` gem but things didn't seem to work at first.
+if Rails.application.secrets.backblaze[:access_key_id].present?
+  require 'carrierwave/storage/fog'
+
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.secrets.backblaze[:access_key_id],
+      aws_secret_access_key: Rails.application.secrets.backblaze[:secret_access_key],
+      endpoint: 'https://s3.us-west-001.backblazeb2.com'
+    }
+    config.fog_directory  = 'cercles-coop-production'
+    config.fog_public     = true
+    config.fog_attributes = {
+      'Cache-Control' => "max-age=#{365.day.to_i}",
+      'X-Content-Type-Options' => 'nosniff'
+    }
+  end
+end
