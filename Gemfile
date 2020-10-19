6588d8f8803efd81894e14df4f7e43a03e0f69cb
Pau Perez <saulopefa@gmail.com>
2020-10-19 11:22:40 +0200

    Make Carrierwave work with Backblaze's B2
    Going through Fog AWS I realized that `:endpoint` is just an alternative
    to providing `:host`, `:scheme` and `:port`.
    
    It must be that the `@endpoint` set in
    fog-aws-3.6.7/lib/fog/aws/storage.rb is used elsewhere because replacing
    `:endpoint` with these other config vars doesn't work, although their
    equivalent ivars do end up with the same value.

--- Gemfile
+++ Gemfile
@@ -33,7 +33,7 @@ end
 
 group :production do
   gem 'dalli'
-  gem 'fog-backblaze'
+  gem 'fog-aws'
   gem 'lograge'
   gem 'sentry-raven'
   gem 'sidekiq'
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,17 +11,23 @@ end
 
 # Needs the "fog-backblaze" gem.
 #
-CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_credentials = {
-    provider: 'backblaze',
-    b2_key_id: Rails.application.secrets.backblaze.access_key_id,
-    b2_key_token: Rails.application.secrets.backblaze.secret_access_key
-  }
-  config.fog_directory  = 'cercles-coop-production'
-  config.fog_public     = true
-  config.fog_attributes = {
-    'Cache-Control' => "max-age=#{365.day.to_i}",
-    'X-Content-Type-Options' => "nosniff"
-  }
+if Rails.application.secrets.backblaze[:access_key_id].present?
+  require "carrierwave/storage/fog"
+
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.secrets.backblaze[:access_key_id],
+      aws_secret_access_key: Rails.application.secrets.backblaze[:secret_access_key],
+      endpoint: 'https://s3.us-west-001.backblazeb2.com'
+    }
+    config.fog_directory  = 'cercles-coop-production'
+    config.fog_public     = true
+    config.fog_attributes = {
+      'Cache-Control' => "max-age=#{365.day.to_i}",
+      'X-Content-Type-Options' => "nosniff"
+    }
+  end
 end
