54a33b542ceeecacb506e0092f090208f75d30e4
Pau Perez <saulopefa@gmail.com>
2021-06-16 18:25:49 +0200

    Configure Sentry from an initializer
    It seems that since
    https://github.com/coopdevs/decidim-coopcat/pull/146 (I have no actual
    proof) no errors are being reported.
    
    I noticed that the DSN wasn't matching the one shown in Sentry's
    settings but that wasn't enough. Calling `init` manually may fix it.
    
    This is copied from
    https://github.com/AjuntamentdeBarcelona/decidim-barcelona/commit/61dd99232e91211b0acb91d4c72f6643339a8e93.

--- app/controllers/decidim_controller.rb
+++ app/controllers/decidim_controller.rb
@@ -7,8 +7,9 @@ class DecidimController < ApplicationController
   private
 
   def set_raven_context
-    return unless Rails.application.secrets.sentry_enabled?
-    Raven.user_context({id: try(:current_user).try(:id)}.merge(session))
-    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+    return unless Rails.application.secrets.sentry_enabled
+
+    Sentry.set_user({id: try(:current_user).try(:id)}.merge(session))
+    Sentry.set_extras(params: params.to_unsafe_h, url: request.url)
   end
 end
