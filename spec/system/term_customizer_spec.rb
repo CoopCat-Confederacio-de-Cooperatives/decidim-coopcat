f5f024bbed825e60350817eeed63e9186d858736
Vera Rojman <vera@platoniq.net>
2021-08-31 14:10:42 +0200

    Add and install Term Customizer (#190)
    * Add and install Term Customizer
    
    * Add spec for term customizer admin page
    
    * Add basic homepage spec

---
+++ spec/system/term_customizer_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Term customizer', type: :system do
+  let(:organization) { create(:organization) }
+  let!(:user) do
+    create(:user, :admin, :confirmed,
+           organization: organization,
+           nickname: 'admin') # Else it gives "Nickname is too long" error
+  end
+
+  before do
+    switch_to_host(organization.host)
+    sign_in user
+  end
+
+  it 'allows visiting the admin page for Term Customizer' do
+    visit decidim_admin.root_path
+    expect(page).to have_link 'Term customizer', href: decidim_admin.root_path + 'term_customizer/sets'
+  end
+end
