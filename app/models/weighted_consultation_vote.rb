1830897ba2d5d056a1a250c28da76228b5e7ef1b
Vera Rojman <vrojman@protonmail.com>
2022-06-21 17:09:00 +0200

    Use default metadata hash in export consultations results job (#233)
    * Use default metadata hash in export consultations results job
    
    * Improve error logging

--- app/models/weighted_consultation_vote.rb
+++ app/models/weighted_consultation_vote.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class WeightedConsultationVote < ApplicationRecord
+  belongs_to :consultation, class_name: "Decidim::Consultation", foreign_key: "decidim_consultation_id"
+  belongs_to :question, class_name: "Decidim::Consultations::Question", foreign_key: "decidim_consultation_question_id"
+  belongs_to :response, class_name: "Decidim::Consultations::Response", foreign_key: "decidim_consultations_response_id"
 end
