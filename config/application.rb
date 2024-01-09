ba494f6f0439e4b3c5e06ebfbab90b0aaa593fbf
Anna Topalidi <60363870+antopalidi@users.noreply.github.com>
2024-01-09 12:55:40 +0100

    upgrade 0.27.4 (#307)
    * update action delegator
    
    * upgrade 27
    
    * add missing envs
    
    * update awesome
    
    * fixes
    
    * Squashed commit of the following:
    
    commit c212555e7e036294de93ec2843a67c0997237ce6
    Author: Ivan Vergés <ivan@pokecode.net>
    Date:   Tue Nov 14 12:31:12 2023 +0100
    
        fixes
    
    commit 70452d97e4dd676aab8ba3ca8918ab4ef683dac3
    Author: Ivan Vergés <ivan@pokecode.net>
    Date:   Thu Nov 9 17:04:17 2023 +0100
    
        update awesome
    
    commit a1e23cf6f89d4ee9e7c2f78e5daf8c116eb6d194
    Author: Ivan Vergés <ivan@pokecode.net>
    Date:   Thu Nov 9 12:00:11 2023 +0100
    
        add missing envs
    
    commit 9590a684f18fb5f0c25836d35b2ed11f932fa313
    Author: Anna Topalidi <t.opalidisanna@gmail.com>
    Date:   Wed Oct 11 11:00:29 2023 +0200
    
        upgrade 27
    
    commit 566683e0d3abd52f533ad99632691f8d6e6052c5
    Author: Ivan Vergés <ivan@pokecode.net>
    Date:   Thu Jul 27 18:19:34 2023 +0200
    
        update awesome
    
    commit 2e180a648a1625b14314af1745b0059af1e1c52d
    Author: Ivan Vergés <ivan@pokecode.net>
    Date:   Thu Jul 27 17:46:58 2023 +0200
    
        update awesome
    
    commit 7500574704c19f160725859a96c7c0598e21b6fa
    Author: Ivan Vergés <ivan@pokecode.net>
    Date:   Thu Jul 27 14:46:29 2023 +0200
    
        enable admin accountability
    
    commit dff35fa6b16eaa3884c72af0d858dfe0c53422b3
    Author: Ivan Vergés <ivan@pokecode.net>
    Date:   Thu Jul 27 14:33:07 2023 +0200
    
        update awesome
    
    commit 08741251c106ca7be3c0e979f86949c1d250b5cd
    Author: Ivan Vergés <ivan@pokecode.net>
    Date:   Wed Jun 7 11:35:35 2023 +0200
    
        update action delegator
    
    * fix dockerfile
    
    * Squashed commit of the following:
    
    commit 637cacdd1f581ca77f5acaac15edfcf42f731b21
    Author: Ivan Vergés <ivan@pokecode.net>
    Date:   Tue Nov 14 12:40:57 2023 +0100
    
        fix dockerfile
    
    commit c212555e7e036294de93ec2843a67c0997237ce6
    Author: Ivan Vergés <ivan@pokecode.net>
    Date:   Tue Nov 14 12:31:12 2023 +0100
    
        fixes
    
    commit 70452d97e4dd676aab8ba3ca8918ab4ef683dac3
    Author: Ivan Vergés <ivan@pokecode.net>
    Date:   Thu Nov 9 17:04:17 2023 +0100
    
        update awesome
    
    commit a1e23cf6f89d4ee9e7c2f78e5daf8c116eb6d194
    Author: Ivan Vergés <ivan@pokecode.net>
    Date:   Thu Nov 9 12:00:11 2023 +0100
    
        add missing envs
    
    commit 9590a684f18fb5f0c25836d35b2ed11f932fa313
    Author: Anna Topalidi <t.opalidisanna@gmail.com>
    Date:   Wed Oct 11 11:00:29 2023 +0200
    
        upgrade 27
    
    commit 566683e0d3abd52f533ad99632691f8d6e6052c5
    Author: Ivan Vergés <ivan@pokecode.net>
    Date:   Thu Jul 27 18:19:34 2023 +0200
    
        update awesome
    
    commit 2e180a648a1625b14314af1745b0059af1e1c52d
    Author: Ivan Vergés <ivan@pokecode.net>
    Date:   Thu Jul 27 17:46:58 2023 +0200
    
        update awesome
    
    commit 7500574704c19f160725859a96c7c0598e21b6fa
    Author: Ivan Vergés <ivan@pokecode.net>
    Date:   Thu Jul 27 14:46:29 2023 +0200
    
        enable admin accountability
    
    commit dff35fa6b16eaa3884c72af0d858dfe0c53422b3
    Author: Ivan Vergés <ivan@pokecode.net>
    Date:   Thu Jul 27 14:33:07 2023 +0200
    
        update awesome
    
    commit 08741251c106ca7be3c0e979f86949c1d250b5cd
    Author: Ivan Vergés <ivan@pokecode.net>
    Date:   Wed Jun 7 11:35:35 2023 +0200
    
        update action delegator
    
    * udpate dockerfile
    
    * recover removed files
    
    * update
    
    * update awesome
    
    * upgrade awesome
    
    * fix tests
    
    * update awesome
    
    * update 0.27.5
    
    * use branch
    
    ---------
    
    Co-authored-by: Ivan Vergés <ivan@pokecode.net>

--- config/application.rb
+++ config/application.rb
@@ -7,22 +7,15 @@ require "decidim/rails"
 require "action_cable/engine"
 # require "action_mailbox/engine"
 # require "action_text/engine"
-require_relative "../lib/decidim/env"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Decidim
-  def self.module_installed?(mod)
-    Gem.loaded_specs.has_key?("decidim-#{mod}")
-  end
-end
-
 module DecidimApplication
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
