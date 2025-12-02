18882170d3f1deefe098d662f42ec60eafd9f798
David Igón <david@pokecode.net>
2025-12-02 12:29:45 +0100

    remove caprover assets path

--- config/assets.rb
+++ config/assets.rb
@@ -3,6 +3,4 @@
 base_path = File.expand_path("..", __dir__)
 
 Decidim::Shakapacker.register_path("#{base_path}/app/packs")
-Decidim::Shakapacker.register_entrypoints(
-  caprover_system: "#{base_path}/app/packs/entrypoints/caprover_system.js"
-)
+
