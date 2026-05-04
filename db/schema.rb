01eaacd78d10fd7f87a3038586fe9dce0ce9fcfd
Ivan Vergés <ivan@pokecode.net>
2026-05-04 16:09:36 +0200

    update decidim & gems

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_30_133943) do
+ActiveRecord::Schema[7.2].define(version: 2026_05_04_140912) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
   enable_extension "pg_trgm"
@@ -464,7 +464,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_30_133943) do
     t.jsonb "title"
     t.integer "weight", default: 0, null: false
     t.jsonb "description"
-    t.integer "total_budget", default: 0
+    t.bigint "total_budget", default: 0
     t.integer "decidim_component_id"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
